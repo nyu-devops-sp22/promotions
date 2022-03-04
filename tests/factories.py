c3767ebfbd066195d46e6bb0915b63b70922209a
andrewzhao <yaoz@gmail.com>
2022-03-04 04:45:44 +0000

    add some model methods used for CRUD operations

---
+++ tests/factories.py
@@ -0,0 +1,31 @@
+"""
+Test Factory to make fake objects for testing
+"""
+from datetime import datetime, timezone
+
+import factory
+from factory.fuzzy import FuzzyChoice, FuzzyDateTime, FuzzyFloat
+from service.models import Promotion, Type
+
+
+class PromotionFactory(factory.Factory):
+    """Creates fake promotions"""
+
+    class Meta:
+        """Maps factory to data model"""
+        model = Promotion
+
+    id = factory.Sequence(lambda n: n)
+    name = factory.Faker("name")
+    start_date = FuzzyDateTime(
+        datetime(2022, 1, 1, tzinfo=timezone.utc),
+        datetime(2022, 1, 20, 20, tzinfo=timezone.utc),
+    )
+    end_date = FuzzyDateTime(
+        datetime(2022, 1, 21, tzinfo=timezone.utc),
+        datetime(2022, 2, 27, tzinfo=timezone.utc),
+    )
+    type = FuzzyChoice(choices=[Type.Value, Type.Percentage, Type.Unknown])
+    value = FuzzyFloat(1.0, 99.0)
+    ongoing = FuzzyChoice(choices=[True, False])
+    product_id = factory.Sequence(lambda n: n)
