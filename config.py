d325c88bf1d2a0ab67237ee566f682f9d711dc72
tinay0313 <tina.yuan.1328@gmail.com>
2022-04-01 10:42:40 -0400

    added if clause to fetch database_uri

--- config.py
+++ config.py
@@ -8,13 +8,16 @@ import logging
 # Get configuration from environment
 DATABASE_URI = os.getenv(
     "DATABASE_URI",
-    "postgres://postgres:postgres@localhost:5432/postgres"
+    "postgresql://postgres:postgres@localhost:5432/postgres"
 )
 
 # override if we are running in Cloud Foundry
 if 'VCAP_SERVICES' in os.environ:
     vcap = json.loads(os.environ['VCAP_SERVICES'])
     DATABASE_URI = vcap['user-provided'][0]['credentials']['url']
+elif 'DATABASE_URI' in os.environ:
+    # Get the credentials from DATABASE_URI
+    database_uri = os.environ['DATABASE_URI']
 
 # Configure SQLAlchemy
 SQLALCHEMY_DATABASE_URI = DATABASE_URI
