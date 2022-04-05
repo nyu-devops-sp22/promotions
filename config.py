f23ab63de129490bbff8bd479c77b8048b0527d3
tinay0313 <tina.yuan.1328@gmail.com>
2022-04-05 19:30:42 +0200

    replaced postgres:// with postgresql:// when retrieving vcaps url

--- config.py
+++ config.py
@@ -14,7 +14,10 @@ DATABASE_URI = os.getenv(
 # override if we are running in Cloud Foundry
 if 'VCAP_SERVICES' in os.environ:
     vcap = json.loads(os.environ['VCAP_SERVICES'])
-    DATABASE_URI = vcap['user-provided'][0]['credentials']['url']
+    uri = vcap['user-provided'][0]['credentials']['url']
+    if uri and uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    DATABASE_URI = uri
 
 # Configure SQLAlchemy
 SQLALCHEMY_DATABASE_URI = DATABASE_URI
